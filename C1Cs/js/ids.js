50248e8cbc0626a65bd05cbcfa88dee910923fc5
room111 <sharongawin@Sharons-Air.attlocal.net>
2017-04-04 22:09:25 -0500

    Revamp

---
+++ C1Cs/js/ids.js
@@ -0,0 +1,177 @@
+var ids = {
+  passwords: ["gnnkg4811","rcwn2941","nkpfuga4811","yknn4777","qygp4777",
+  "uwoogt5026","tggf2753","lquj2954","uctc4806","cdda4811",
+  "ocz4806","dtcpfqp4806","ocffkg4811","cngz4806","lqtfcp4861",
+  "pgungt81","goknkg5568","kcp4781","cnckpc5107","nqnc4967",
+  "ctk4811","cffkuqp4806","ctk8057","uknnaiqcv","cxk2753"],
+  people: {
+    "ellie4811": {
+         name: "Ellie",
+         balance: "11.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 11.00/20.00"
+    },"paul2941": {
+         name: "Paul",
+         balance: "5.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 5.00/20.00"
+    },"lindsey4811": {
+         name: "Lindsey",
+         balance: "12.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 12.00/20.00"
+    },"will4777": {
+         name: "Will",
+         balance: "11.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 11.00/20.00"
+    },"owen4777": {
+         name: "Owen",
+         balance: "7.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 7.00/20.00"
+    },"summer5026": {
+         name: "Summer",
+         balance: "11.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 11.00/20.00"
+    },"reed2753": {
+         name: "Reed",
+         balance: "6.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 6.00/20.00"
+    },"josh2954": {
+         name: "Josh",
+         balance: "5.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 5.00/20.00"
+    },"sara4806": {
+         name: "Sara",
+         balance: "3.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 9.00/20.00"
+    },"abby4811": {
+         name: "Abby",
+         balance: "00.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 0.00/20.00"
+    },"max4806": {
+         name: "Max",
+         balance: "12.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 12.00/20.00"
+    },"brandon4806": {
+         name: "Brandon",
+         balance: "13.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 13.00/20.00"
+    },"maddie4811": {
+         name: "Maddie",
+         balance: "7.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 7.00/20.00"
+    },"alex4806": {
+         name: "Alex",
+         balance: "10.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 10.00/20.00"
+    },"jordan4861": {
+         name: "Jordan",
+         balance: "8.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 8.00/20.00"
+    },"nesler81": {
+         name: "Josh",
+         balance: "5.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 5.00/20.00"
+    },"emilie5568": {
+         name: "Emilie",
+         balance: "5.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 5.00/20.00"
+    },"ian4781": {
+         name: "Ian",
+         balance: "5.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 5.00/20.00"
+    },"alaina5107": {
+         name: "Alaina",
+         balance: "5.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 5.00/20.00"
+    },"lola4967": {
+         name: "Lola",
+         balance: "5.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 5.00/20.00"
+    },"ari4811": {
+         name: "Ari",
+         balance: "10.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 15.00/20.00"
+    },"addison4806": {
+         name: "Addison",
+         balance: "4.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 4.00/20.00"
+    },"ari8057": {
+         name: "Ari",
+         balance: "5.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 5.00/20.00"
+    },"sillygoat": {
+         name: "Drew",
+         balance: "11.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 11.00/20.00"
+    },"avi2753": {
+         name: "Avi",
+         balance: "5.00",
+         compliment: "Give three compliments. 0/3",
+         jokes: "Share 2 funny jokes. 0/2",
+         money: "Earn 20 C1Cs. 5.00/20.00"
+    },
+  }
+}
+var i = 1;
+var key;
+var person;
+for (key in ids.people){
+  if(key == location.hash.replace("#","")){
+    person=ids.people[key]
+  }
+  else {
+  }
+}
+$("[data-replace=name]").text($("[data-replace=name]").text().replace("{{}}", person.name))
+alert($("[data-replace=name]").text());
+$("[data-replace=balance]").html($("[data-replace=balance]").html().replace("{{}}", person.balance))
+$("[data-replace=compliment]").text($("[data-replace=compliment]").text().replace("{{}}", person.compliment ))
+$("[data-replace=jokes]").text($("[data-replace=jokes]").text().replace("{{}}", person.jokes ))
+$("[data-replace=money]").text($("[data-replace=money]").text().replace("{{}}", person.money ));
+console.log(JSON.stringify(person))
---
+++ C1Cs/js/main.js
@@ -0,0 +1,293 @@
+/**
+ * main.js
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright 2015, Codrops
+ * http://www.codrops.com
+ */
+;(function(window) {
+
+	'use strict';
+
+	var support = { transitions: Modernizr.csstransitions },
+		// transition end event name
+		transEndEventNames = { 'WebkitTransition': 'webkitTransitionEnd', 'MozTransition': 'transitionend', 'OTransition': 'oTransitionEnd', 'msTransition': 'MSTransitionEnd', 'transition': 'transitionend' },
+		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+		onEndTransition = function( el, callback ) {
+			var onEndCallbackFn = function( ev ) {
+				if( support.transitions ) {
+					if( ev.target != this ) return;
+					this.removeEventListener( transEndEventName, onEndCallbackFn );
+				}
+				if( callback && typeof callback === 'function' ) { callback.call(this); }
+			};
+			if( support.transitions ) {
+				el.addEventListener( transEndEventName, onEndCallbackFn );
+			}
+			else {
+				onEndCallbackFn();
+			}
+		},
+		// the pages wrapper
+		stack = document.querySelector('.pages-stack'),
+		// the page elements
+		pages = [].slice.call(stack.children),
+		// total number of page elements
+		pagesTotal = pages.length,
+		// index of current page
+		current = 0,
+		// menu button
+		menuCtrl = document.querySelector('button.menu-button'),
+		// the navigation wrapper
+		nav = document.querySelector('.pages-nav'),
+		// the menu nav items
+		navItems = [].slice.call(nav.querySelectorAll('.link--page')),
+		// check if menu is open
+		isMenuOpen = false;
+
+	function init() {
+		buildStack();
+		initEvents();
+	}
+
+	function buildStack() {
+		var stackPagesIdxs = getStackPagesIdxs();
+
+		// set z-index, opacity, initial transforms to pages and add class page--inactive to all except the current one
+		for(var i = 0; i < pagesTotal; ++i) {
+			var page = pages[i],
+				posIdx = stackPagesIdxs.indexOf(i);
+
+			if( current !== i ) {
+				classie.add(page, 'page--inactive');
+
+				if( posIdx !== -1 ) {
+					// visible pages in the stack
+					page.style.WebkitTransform = 'translate3d(0,100%,0)';
+					page.style.transform = 'translate3d(0,100%,0)';
+				}
+				else {
+					// invisible pages in the stack
+					page.style.WebkitTransform = 'translate3d(0,75%,-300px)';
+					page.style.transform = 'translate3d(0,75%,-300px)';
+				}
+			}
+			else {
+				classie.remove(page, 'page--inactive');
+			}
+
+			page.style.zIndex = i < current ? parseInt(current - i) : parseInt(pagesTotal + current - i);
+
+			if( posIdx !== -1 ) {
+				page.style.opacity = parseFloat(1 - 0.1 * posIdx);
+			}
+			else {
+				page.style.opacity = 0;
+			}
+		}
+	}
+
+	// event binding
+	function initEvents() {
+		// menu button click
+		menuCtrl.addEventListener('click', toggleMenu);
+
+		// navigation menu clicks
+		navItems.forEach(function(item) {
+			// which page to open?
+			var pageid = item.getAttribute('href').slice(1);
+			item.addEventListener('click', function(ev) {
+				ev.preventDefault();
+				openPage(pageid);
+			});
+		});
+
+		// clicking on a page when the menu is open triggers the menu to close again and open the clicked page
+		pages.forEach(function(page) {
+			var pageid = page.getAttribute('id');
+			page.addEventListener('click', function(ev) {
+				if( isMenuOpen ) {
+					ev.preventDefault();
+					openPage(pageid);
+				}
+			});
+		});
+
+		// keyboard navigation events
+		document.addEventListener( 'keydown', function( ev ) {
+			if( !isMenuOpen ) return;
+			var keyCode = ev.keyCode || ev.which;
+			if( keyCode === 27 ) {
+				closeMenu();
+			}
+		} );
+	}
+
+	// toggle menu fn
+	function toggleMenu() {
+		if( isMenuOpen ) {
+			closeMenu();
+		}
+		else {
+			openMenu();
+			isMenuOpen = true;
+		}
+	}
+
+	// opens the menu
+	function openMenu() {
+		// toggle the menu button
+		classie.add(menuCtrl, 'menu-button--open')
+		// stack gets the class "pages-stack--open" to add the transitions
+		classie.add(stack, 'pages-stack--open');
+		// reveal the menu
+		classie.add(nav, 'pages-nav--open');
+
+		// now set the page transforms
+		var stackPagesIdxs = getStackPagesIdxs();
+		for(var i = 0, len = stackPagesIdxs.length; i < len; ++i) {
+			var page = pages[stackPagesIdxs[i]];
+			page.style.WebkitTransform = 'translate3d(0, 75%, ' + parseInt(-1 * 200 - 50*i) + 'px)'; // -200px, -230px, -260px
+			page.style.transform = 'translate3d(0, 75%, ' + parseInt(-1 * 200 - 50*i) + 'px)';
+		}
+	}
+
+	// closes the menu
+	function closeMenu() {
+		// same as opening the current page again
+		openPage();
+	}
+
+	// opens a page
+	function openPage(id) {
+		var futurePage = id ? document.getElementById(id) : pages[current],
+			futureCurrent = pages.indexOf(futurePage),
+			stackPagesIdxs = getStackPagesIdxs(futureCurrent);
+
+		// set transforms for the new current page
+		futurePage.style.WebkitTransform = 'translate3d(0, 0, 0)';
+		futurePage.style.transform = 'translate3d(0, 0, 0)';
+		futurePage.style.opacity = 1;
+
+		// set transforms for the other items in the stack
+		for(var i = 0, len = stackPagesIdxs.length; i < len; ++i) {
+			var page = pages[stackPagesIdxs[i]];
+			page.style.WebkitTransform = 'translate3d(0,100%,0)';
+			page.style.transform = 'translate3d(0,100%,0)';
+		}
+
+		// set current
+		if( id ) {
+			current = futureCurrent;
+		}
+
+		// close menu..
+		classie.remove(menuCtrl, 'menu-button--open');
+		classie.remove(nav, 'pages-nav--open');
+		onEndTransition(futurePage, function() {
+			classie.remove(stack, 'pages-stack--open');
+			// reorganize stack
+			buildStack();
+			isMenuOpen = false;
+		});
+	}
+
+	// gets the current stack pages indexes. If any of them is the excludePage then this one is not part of the returned array
+	function getStackPagesIdxs(excludePageIdx) {
+		var nextStackPageIdx = current + 1 < pagesTotal ? current + 1 : 0,
+			nextStackPageIdx_2 = current + 2 < pagesTotal ? current + 2 : 1,
+			idxs = [],
+
+			excludeIdx = excludePageIdx || -1;
+
+		if( excludePageIdx != current ) {
+			idxs.push(current);
+		}
+		if( excludePageIdx != nextStackPageIdx ) {
+			idxs.push(nextStackPageIdx);
+		}
+		if( excludePageIdx != nextStackPageIdx_2 ) {
+			idxs.push(nextStackPageIdx_2);
+		}
+
+		return idxs;
+	}
+
+	init();
+
+})(window);
+
+//ids
+
+var caesarShift = function(str, amount) {
+
+  // Wrap the amount
+  if (amount < 0)
+    return caesarShift(str, amount + 26);
+
+  // Make an output variable
+  var output = '';
+
+  // Go through each character
+  for (var i = 0; i < str.length; i ++) {
+
+    // Get the character we'll be appending
+    var c = str[i];
+
+    // If it's a letter...
+    if (c.match(/[a-z]/i)) {
+
+      // Get its code
+      var code = str.charCodeAt(i);
+
+      // Uppercase letters
+      if ((code >= 65) && (code <= 90))
+        c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
+
+      // Lowercase letters
+      else if ((code >= 97) && (code <= 122))
+        c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+
+    }
+
+    // Append
+    output += c;
+
+  }
+
+  // All done!
+  return output;
+
+};
+$(".submit, .submit *").click(function(){
+  var current = false, pass;
+  for (var i = 0; ids.passwords.length > i; i++) {
+    if(caesarShift(ids.passwords[i],-2) == $(".passcode").val()){
+      current = true;
+      pass = caesarShift(ids.passwords[i],-2);
+    }
+    else {
+    }
+  }
+
+  if(current) {
+    $(".passcode").removeClass("error");
+    $(".passcode").addClass("success");
+		$(".submit").fadeOut();
+    $(".passcode").val("Loading your account...")
+    setTimeout(function(){
+      setTimeout(function(){
+        $("html").addClass("animated");
+        $("html").addClass("fadeOut");
+      }, 2000)
+			$(".passcode").val("")
+      location.href = "account.html#"+pass;
+    }, 2000)
+  }
+  else {
+    $(".passcode").addClass("error");
+  }
+})
